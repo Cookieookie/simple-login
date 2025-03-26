ab5cfff78c2b68246e63a4c705457a4ae555b70b
Cookie Van <cookievan03@gmail.com>
2025-03-26 17:29:18 -0500

    adjusted some placment of elements with bootstrap

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,21 +1,22 @@
 import React from 'react'
 
+
 function Login() {
     return(
         <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
-            <div>
-                <form action="bg-white p-3 rounded w-25">
+            <div className='bg-white p-3 rounded w-25'>
+                <form action="">
                     <div className='mb-3'>
-                        <label htmlFor="email">Email</label>
-                        <input type="email" placeholder="Enter Email"/>
+                        <label htmlFor="email"><strong>Email</strong></label>
+                        <input type="email" placeholder="Enter Email" className='form-control rounded-0'/>
                     </div>
                     <div className='mb-3'>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" placeholder="Password"/>
+                        <label htmlFor="password"><strong>Password</strong></label>
+                        <input type="password" placeholder="Password" className='form-control rounded-0'/>
                     </div>
-                    <button className='btn btn-success'>Log in</button>
-                    <p></p>
-                    <button className="btn btn-default border">Create Account</button>
+                    <button className='btn btn-success w-100 rounded-0'>Log in</button>
+                    <p className='text-sm-center'>Don't have an account yet?</p>
+                    <button className="btn btn-default border w-100 bg-light rounded-0">Create Account</button>
                 
                 </form>
             </div>
