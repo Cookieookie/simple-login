617b5b8e8cebdb81a6161ae70b721c23c106bebb
Cookie Van <cookievan03@gmail.com>
2025-04-08 00:07:57 -0500

    added handleSubmit fxn for form. working on some bugs now.

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,18 +1,34 @@
-import React from 'react'
-
+import react from 'react'
+import axios from 'axios';
+// import 'bootstrap/dist/css/boostrap.min.css';
+import { useState } from 'react';
 
 function Login() {
+    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        axios.post('http://localhost:8081/login', {email, password})
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    }
+
+
     return(
-        <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+        <div className='d-flex vh-100 justify-content-center align-items-center bg-primary vh-100'>
             <div className='bg-white p-3 rounded w-25'>
-                <form action="">
+                <form onSubmit={handleSubmit}>
                     <div className='mb-3'>
                         <label htmlFor="email"><strong>Email</strong></label>
-                        <input type="email" placeholder="Enter Email" className='form-control rounded-0'/>
+                        <input type="email" placeholder="Enter Email" className='form-control rounded-0'
+                        onChange={e => setEmail(e.target.value)}/>
                     </div>
                     <div className='mb-3'>
                         <label htmlFor="password"><strong>Password</strong></label>
-                        <input type="password" placeholder="Password" className='form-control rounded-0'/>
+                        <input type="password" placeholder="Password" className='form-control rounded-0'
+                        onChange={e => setPassword(e.target.value)}/>
                     </div>
                     <button className='btn btn-success w-100 rounded-0'>Log in</button>
                     <p className='text-sm-center'>Don't have an account yet?</p>
